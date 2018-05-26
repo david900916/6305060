c2abc795dcbcbc883271558204740a6ec48cae2c
david900916 <david900916@yahoo.com.tw>
2018-05-26 15:23:22 +0800

    master

---
+++ post.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>表單資料 :</h2>
+<form action="test.php" method="POST">
+		<input type="text" name="name" value="" placeholder="名稱">
+		<input type="text" name="account" value="" placeholder="帳號">
+		<input type="password" name="password" value="" placeholder="密碼">
+		<input type="submit">
+	</form>
+	</body>
