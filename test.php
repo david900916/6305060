54ef1afbf9b511379329ab7629c9be481b76e1a8
david900916 <david900916@yahoo.com.tw>
2018-05-26 16:00:56 +0800

    master

--- test.php
+++ test.php
@@ -1,3 +1,11 @@
 <?php
-print_r($_POST);
+
+$dsn = "mysql:host=192.168.32.90;dbname=dbname01";
+$db = new PDO($dsn, "dbname01","dbname@pwd");
+$rs =$db->query("SELECT * FROM users WHERE account = '$_POST[name]' and password = '$_POST[password]'  ");
+$result = $rs ->fetch();
+if ($result){
+echo"正確";print_r($_POST);}
+else
+	echo"錯誤";
 ?>
