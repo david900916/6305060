472791a2a92d3229d380d117baba1d2483d1f786
david900916 <david900916@yahoo.com.tw>
2018-05-26 16:02:47 +0800

    master

--- post.php
+++ post.php
@@ -3,8 +3,9 @@
 <h2>表單資料 :</h2>
 <form action="test.php" method="POST">
 		<input type="text" name="name" value="" placeholder="名稱">
-		<input type="text" name="account" value="" placeholder="帳號">
+		<!--<input type="text" name="account" value="" placeholder="帳號">-->
 		<input type="password" name="password" value="" placeholder="密碼">
+		<!--<input type="date" name="number" value="" placeholder="編號">-->
 		<input type="submit">
 	</form>
 	</body>
